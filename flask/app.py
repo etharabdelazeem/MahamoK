d09a18182362541ac957a6b6211022c7b6307d9c
etharabdelazeem <etharabdelazeem1@gmail.com>
2024-06-21 06:22:49 +0000

    Added configuration for Mysql Database, mahamok database, user and task tables

--- flask/app.py
+++ flask/app.py
@@ -1,6 +1,14 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for
-#from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
+from sqlalchemy.sql import func
+
+from config import DevelopmentConfig, ProductionConfig
+from dotenv import load_dotenv
+load_dotenv()  # Load environment variables from .env file
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
@@ -12,6 +20,15 @@ def Welcome():
 @app.route('/homepage')
 def homepage():
     return render_template('homepage.html')
+
+# Database configuration
+if os.environ.get('FLASK_ENV') == 'production':
+    app.config.from_object(ProductionConfig)
+else:
+    app.config.from_object(DevelopmentConfig)
+
+# Initialize the database
+db = SQLAlchemy(app)
 
 if __name__ == '__main__':
         app.run(debug=True, host='0.0.0.0', port='5000')
---
+++ flask/config.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+load_dotenv(os.path.join(basedir, '.env'))
+
+class Config:
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'mysql+pymysql://mahamok_user:your_password@localhost/mahamok'
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://prod_user:prod_password@db.example.com/mahamok'
