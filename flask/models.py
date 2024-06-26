f39743ad92f5d4f5972b526770b113199d36c5fd
etharabdelazeem <etharabdelazeem1@gmail.com>
2024-06-26 20:14:59 +0000

    Add more fields to the Task and user model to handle deadlines and priorities

--- flask/models.py
+++ flask/models.py
@@ -1,14 +1,20 @@
+from datetime import datetime
 from app import db
 from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
+    tasks = db.relationship('Task', backref='user', lazy=True)
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150), nullable=False)
     description = db.Column(db.String(500))
+    deadline = db.Column(db.DateTime)
+    priority = db.Column(db.String(50))  # e.g., "low", "medium", "high"
     completed = db.Column(db.Boolean, default=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
